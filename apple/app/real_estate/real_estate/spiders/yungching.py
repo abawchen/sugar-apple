6578b113ff49bd3e39f5966b67c9c4d40c1acebb
abawchen <abaw.chen@gmail.com>
2017-12-11 16:24:20 +0800

    feat: Finish crawler by page.

--- apple/app/real_estate/real_estate/spiders/yungching.py
+++ apple/app/real_estate/real_estate/spiders/yungching.py
@@ -7,7 +7,7 @@ class YungchingSpider(scrapy.Spider):
     name = 'yungching'
     allowed_domains = ['buy.yungching.com.tw']
     start_urls = [
-        'https://buy.yungching.com.tw/region/台北市-_c/'
+        'https://buy.yungching.com.tw/region/台北市-_c/?pg=345'
     ]
 
     def parse(self, response):
@@ -24,8 +24,20 @@ class YungchingSpider(scrapy.Spider):
             item['sn'], item['description'] = description.split('  ', maxsplit=1)
 
             detail = record.css('ul.item-info-detail')
+            details = [v.strip() for v in detail.css('li::text').extract()]
+            if len(details) < 9:
+                details += [''] * (9 - len(details))
             item['category'], item['age'], item['floor'], \
             item['land_ping'], item['main_ping'], item['building_ping'], \
             item['layout'], item['added_layout'], item['parking_lot'] = \
-                [v.strip() for v in detail.css('li::text').extract()]
+                details
             yield item
+
+        active = response.css('li.is-active')
+        page_no = int(re.findall('pg=(\d+)', response.url)[0])
+        disabled = response.css('li.disabled')
+        if disabled and disabled.css('a::text').extract_first() == '下一頁 >':
+            return
+
+        next_page_url = re.sub('pg=(\d+)', 'pg=' + str(page_no + 1), response.url)
+        yield scrapy.Request(url=next_page_url)
