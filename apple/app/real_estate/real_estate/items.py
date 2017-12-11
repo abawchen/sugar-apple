8a7627a25cc276983f28eb09a34f45c70f31b28e
abawchen <abaw.chen@gmail.com>
2017-12-11 16:06:13 +0800

    feat: Parsing all item in list view from Yungching.

--- apple/app/real_estate/real_estate/items.py
+++ apple/app/real_estate/real_estate/items.py
@@ -9,12 +9,56 @@ import scrapy
 
 
 class RealEstateItem(scrapy.Item):
-    # define the fields for your item here like:
-    # city = scrapy.Field()
+    # 仲介
+    agent = scrapy.Field()
+
+    # 縣市
     city_name = scrapy.Field()
-    name = scrapy.Field()
+
+    # 區
     area = scrapy.Field()
+
+    # 建案名稱
+    name = scrapy.Field()
+
+    # 地址
     address = scrapy.Field()
-    bid_price = scrapy.Field()
+
+    # 仲介物件編號
+    sn = scrapy.Field()
+
+    # 描述
+    description = scrapy.Field()
+
+    # 開價
+    list_price = scrapy.Field()
+
+    # 公寓、電梯大樓
+    category = scrapy.Field()
+
+    # 屋齡
     age = scrapy.Field()
+
+    # 樓層
+    floor = scrapy.Field()
+
+    # 土地（坪）
+    land_ping = scrapy.Field()
+
+    # 主+陽（坪）
+    main_ping = scrapy.Field()
+
+    # 建物（坪）
+    building_ping = scrapy.Field()
+
+    # 格局, ex: 1房(室)2廳1衛
+    layout = scrapy.Field()
+
+    # 加蓋格局
+    added_layout = scrapy.Field()
+
+    # 車位
+    parking_lot = scrapy.Field()
+
+    # 網址
     url = scrapy.Field()
--- apple/app/real_estate/real_estate/spiders/yungching.py
+++ apple/app/real_estate/real_estate/spiders/yungching.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 from real_estate.items import RealEstateItem
 
@@ -12,10 +13,19 @@ class YungchingSpider(scrapy.Spider):
     def parse(self, response):
         for record in response.css('li.m-list-item'):
             item = RealEstateItem()
+            item['agent'] = self.name
+            item['list_price'] = record.css('span.price-num::text').extract_first().replace(',', '')
+
             title = record.css('h3::text').extract_first()
             item['name'], item['address'] = title.split('　')
-            yield item
+            item['city_name'], item['area'] = re.findall(r'\w{3}', item['address'])[:2]
 
-        # filename = response.url.split('/')[-2] + '.html'
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
+            description = record.css('div.item-description::text').extract_first()
+            item['sn'], item['description'] = description.split('  ', maxsplit=1)
+
+            detail = record.css('ul.item-info-detail')
+            item['category'], item['age'], item['floor'], \
+            item['land_ping'], item['main_ping'], item['building_ping'], \
+            item['layout'], item['added_layout'], item['parking_lot'] = \
+                [v.strip() for v in detail.css('li::text').extract()]
+            yield item
