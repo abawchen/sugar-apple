5b2d8040f7a2e66a4427c3f7c82ced76ed9b4be3
Jeremy Go <jeremy.x.go@gmail.com>
2017-12-15 01:51:49 +0800

    Add react router

--- sugar/src/index.js
+++ sugar/src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'mobx-react';
 import { AppContainer } from 'react-hot-loader';
+import { Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import Stores from './stores';
 import App from './components/App';
 
 const stores = new Stores();
+const history = createBrowserHistory();
 
 const render = Component => {
   ReactDom.render(
     <AppContainer>
       <Provider stores={stores}>
-        <Component/>
+        <Router history={history}>
+          <Route path="/" component={Component}/>
+        </Router>
       </Provider>
     </AppContainer>,
     document.getElementById('root'),
